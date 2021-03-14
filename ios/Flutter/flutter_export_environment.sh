e59cd566cd512e44cf93a287c1b6d46737d80b11
darkgod1802 <bryan.choque.canaza@gmail.com>
2021-03-13 20:50:01 -0400

    update dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/phoenixm/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/phoenixm/Desktop/LocalDevelopment/cloud-functions/GeoFlutterFire"
+export "FLUTTER_ROOT=/Users/darkgod/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/darkgod/Documents/ProyectosFlutter/GeoFlutterFire"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
